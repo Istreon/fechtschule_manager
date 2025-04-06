b23f8d09c6d8a15160d87cb3a73647881a4f3815
Aymeric "Istreon" Hénard <47358771+Istreon@users.noreply.github.com>
2025-04-06 21:42:48 +0200

    Remove the participant from the ranking if they have not entered

--- src/ranking.py
+++ src/ranking.py
@@ -19,14 +19,15 @@ def rankingByParticipationAsFencer(db: DataBase, cat: str = "all"):
 
     for p in participants :
         id = p["id"]
-        nbRecontres = 0
+        nbRencontres = 0
         for r in rencontres :
             if cat!= "all" and r["categorie"]!= cat : 
                 continue
             if(r["id_combattant1"] == id or r["id_combattant2"] == id) :
-                nbRecontres = nbRecontres + 1
-        res = {"id": id, "prenom": p["prenom"], "nom": p["nom"], "score": nbRecontres}
-        results.append(res)
+                nbRencontres = nbRencontres + 1
+        if(nbRencontres > 0 ) : 
+            res = {"id": id, "prenom": p["prenom"], "nom": p["nom"], "score": nbRencontres}
+            results.append(res)
 
     sorted_results = sorted(results, key=lambda x: x["score"], reverse=True)
 
@@ -43,14 +44,15 @@ def rankingByParticipationInRefereeing(db: DataBase, cat: str = "all"):
 
     for p in participants :
         id = p["id"]
-        nbRecontres = 0
+        nbRencontres = 0
         for r in rencontres :
             if cat!= "all" and r["categorie"]!= cat : 
                 continue
             if(r["id_arbitre"] == id or r["id_assesseur"] == id) :
-                nbRecontres = nbRecontres + 1
-        res = {"id": id, "prenom": p["prenom"], "nom": p["nom"], "score": nbRecontres}
-        results.append(res)
+                nbRencontres = nbRencontres + 1
+        if(nbRencontres > 0 ) : 
+            res = {"id": id, "prenom": p["prenom"], "nom": p["nom"], "score": nbRencontres}
+            results.append(res)
 
     sorted_results = sorted(results, key=lambda x: x["score"], reverse=True)
 
@@ -68,17 +70,21 @@ def rankingByTotalLifePoints(db: DataBase, cat: str = "all"):
     for p in participants :
         id = p["id"]
         lifePoints = 0
+        nbRencontres = 0
         for r in rencontres :
             if cat!= "all" and r["categorie"]!= cat : 
                 continue
             if(r["id_combattant1"] == id) :
                 lifePoints = lifePoints + r["score1"]
+                nbRencontres = nbRencontres + 1
                 
             if(r["id_combattant2"] == id) :
                 lifePoints = lifePoints + r["score2"]
+                nbRencontres = nbRencontres + 1
 
-        res = {"id": id, "prenom": p["prenom"], "nom": p["nom"], "score": lifePoints}
-        results.append(res)
+        if(nbRencontres > 0 ) : 
+            res = {"id": id, "prenom": p["prenom"], "nom": p["nom"], "score": lifePoints}
+            results.append(res)
 
     sorted_results = sorted(results, key=lambda x: x["score"], reverse=True)
 
@@ -95,24 +101,23 @@ def rankingByRatioTotalLifePointsToRencontres(db: DataBase, cat: str = "all"):
     for p in participants :
         id = p["id"]
         lifePoints = 0
-        nbRecontres = 0
+        nbRencontres = 0
         for r in rencontres :
             if cat!= "all" and r["categorie"]!= cat : 
                 continue
             if(r["id_combattant1"] == id) :
                 lifePoints = lifePoints + r["score1"]
-                nbRecontres = nbRecontres + 1
+                nbRencontres = nbRencontres + 1
                 
             if(r["id_combattant2"] == id) :
                 lifePoints = lifePoints + r["score2"]
-                nbRecontres = nbRecontres + 1
+                nbRencontres = nbRencontres + 1
 
         ratio = 0
-        if(nbRecontres > 0 ) : 
-            ratio = lifePoints / nbRecontres
-
-        res = {"id": id, "prenom": p["prenom"], "nom": p["nom"], "score": truncate_float(ratio)}
-        results.append(res)
+        if(nbRencontres > 0 ) : 
+            ratio = lifePoints / nbRencontres
+            res = {"id": id, "prenom": p["prenom"], "nom": p["nom"], "score": truncate_float(ratio)}
+            results.append(res)
 
     sorted_results = sorted(results, key=lambda x: x["score"], reverse=True)
 
@@ -129,27 +134,26 @@ def rankingByRatioVictoryToDefeat(db: DataBase, cat: str = "all"):
     for p in participants :
         id = p["id"]
         victories = 0
-        nbRecontres = 0
+        nbRencontres = 0
         for r in rencontres :
             if cat!= "all" and r["categorie"]!= cat : 
                 continue
             if(r["id_combattant1"] == id) :
-                nbRecontres = nbRecontres + 1
+                nbRencontres = nbRencontres + 1
                 if(r["score1"] > 0):
                     victories = victories + 1
                 
                 
             if(r["id_combattant2"] == id) :
-                nbRecontres = nbRecontres + 1
+                nbRencontres = nbRencontres + 1
                 if(r["score2"] > 0):
                     victories = victories + 1
 
         ratio = 0
-        if(nbRecontres > 0 ) : 
-            ratio = victories / nbRecontres
-
-        res = {"id": id, "prenom": p["prenom"], "nom": p["nom"], "score": truncate_float(ratio*100,1)}
-        results.append(res)
+        if(nbRencontres > 0 ) : 
+            ratio = victories / nbRencontres
+            res = {"id": id, "prenom": p["prenom"], "nom": p["nom"], "score": truncate_float(ratio*100,1)}
+            results.append(res)
 
     sorted_results = sorted(results, key=lambda x: x["score"], reverse=True)
 
